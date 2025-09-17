2e0c232c79d7da33895c320a49ee07ba740c8d36
manu-gt-hub <manu-gt-hub@users.noreply.github.com>
2025-09-17 17:06:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+import os
+
+def main():
+    secret = "empty"
+    print(f"El secret antes de acceder a el es: {secret}")
+    
+    secret = os.getenv("SECRET_TEST")
+    
+    if secret:
+        print("✅ El secret fue cargado correctamente.")
+        print(f"El secret despues de acceder a el es: {secret}")
+    else:
+        print("❌ No se pudo cargar el secret.")
+
+if __name__ == "__main__":
+    main()
