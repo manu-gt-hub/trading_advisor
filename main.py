f7a32b8bc304ed65626230e03af828732d3ead02
manu-gt-hub <manu-gt-hub@users.noreply.github.com>
2025-09-17 15:26:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 # Acceder al secret desde la variable de entorno
-my_secret = os.getenv("MY_SECRET")
+my_secret = os.getenv("SECRET_TEST")
 
 # Imprimir el valor (en producción, evita imprimir secretos)
 print(f"El secret es: {my_secret}")
